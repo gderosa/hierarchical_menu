328d2578158e0bca5bdd8af2ebf8703b2d0ca04a
Guido De Rosa <job@guidoderosa.net>
2010-09-23 23:26:47 +0200

    fix a syntax error

--- lib/hmenu/node.rb
+++ lib/hmenu/node.rb
@@ -1,49 +1,48 @@
 require 'tree'
 require 'hmenu/extensions/tree'
 
-  module HMenu
-    class Node < Tree::TreeNode
-      def to_html_ul
-        # TODO: use CSS+Javascript to show/hide subitems on click
-        s = ""
-        if isRoot?
-          s << "<a href=\"/\">Home</a>"
+module HMenu
+  class Node < Tree::TreeNode
+    def to_html_ul
+      # TODO: use CSS+Javascript to show/hide subitems on click
+      s = ""
+      if isRoot?
+        s << "<a href=\"/\">Home</a>"
+      else
+        if content 
+          if content[:href]
+            s << 
+              "<a " << 
+                "title=\"#{content[:desc]}\" " << 
+                "href=\"#{content[:href]}.html\">#{content[:name]}" <<
+              "</a>"
+          elsif content[:name]
+            s << 
+                '<span title="' << (content[:desc] || '') << '">' << 
+                  (content[:name] || '') << 
+                '</span>'
+          end
         else
-          if content 
-            if content[:href]
-              s << 
-                "<a " << 
-                  "title=\"#{content[:desc]}\" " << 
-                  "href=\"#{content[:href]}.html\">#{content[:name]}" <<
-                "</a>"
-            elsif content[:name]
-              s << 
-                  '<span title="' << (content[:desc] || '') << '">' << 
-                    (content[:name] || '') << 
-                  '</span>'
-            end
-          else
-            s << name.capitalize
-          end
+          s << name.capitalize
         end
-        if hasChildren?
-          s += "<ul>"
-          children.sort.each do |child|
-            s += "<li>" << child.to_html_ul << "</li>"
-          end
-          s += "</ul>"
-        end
-        return s
       end
-      def <=>(other) # for sorting
-        n <=> other.n 
-      end
-      def n
-        begin
-          content[:n] ? content[:n] : 0
-        rescue
-          0
+      if hasChildren?
+        s += "<ul>"
+        children.sort.each do |child|
+          s += "<li>" << child.to_html_ul << "</li>"
         end
+        s += "</ul>"
+      end
+      return s
+    end
+    def <=>(other) # for sorting
+      n <=> other.n 
+    end
+    def n
+      begin
+        content[:n] ? content[:n] : 0
+      rescue
+        0
       end
     end
   end
